89c282b40d36426e62ac13ac10ffac5e8c225f82
Franyek <franyek@gmail.com>
2019-07-23 10:40:11 +0200

    feature(argparser)

--- separate_images_by_date/separator.py
+++ separate_images_by_date/separator.py
@@ -1,29 +1,39 @@
+import argparse
 import os
 import datetime
 from PIL import Image
 from shutil import copyfile
-
-source_folder = 'c:\\Users\\frany\\Pictures\\2019.07.05 - 17 Norvégia'
-target_folder = 'c:\\Users\\frany\\Pictures\\Norway'
 
 supported_file_extensions = [
     "jpg"
 ]
 
 
-a = []
-for s in os.listdir(source_folder):
-    filename, fileextension = os.path.splitext(s)
-    if os.path.isfile(os.path.join(source_folder, s)) and fileextension[1:].lower() in supported_file_extensions:
-        d_s = Image.open(os.path.join(source_folder, s))._getexif()[36867]
-        a.append((s, datetime.datetime.strptime(d_s, "%Y:%m:%d %H:%M:%S")))
+def get_arg_parser():
+    parser = argparse.ArgumentParser(description="Image file separator")
+    parser.add_argument('-t', '--target_folder', required=True, type=str, help="Target folder")
+    parser.add_argument('-s', '--source_folder', required=True, type=str, help="Source folder")
+    return parser
 
-separated = {}
-for name, date in a:
-    date_str = "{:02d}.{:02d}.".format(date.month, date.day)
-    if not os.path.isdir(os.path.join(target_folder, date_str)):
-        os.makedirs(os.path.join(target_folder, date_str))
-    copyfile(os.path.join(source_folder, name), os.path.join(target_folder, date_str, name))
 
-print(a)
+def get_file_names_dates(source_folder: str) -> list:
+    a = []
+    for s in os.listdir(source_folder):
+        filename, fileextension = os.path.splitext(s)
+        if os.path.isfile(os.path.join(source_folder, s)) and fileextension[1:].lower() in supported_file_extensions:
+            d_s = Image.open(os.path.join(source_folder, s))._getexif()[36867]
+            a.append((s, datetime.datetime.strptime(d_s, "%Y:%m:%d %H:%M:%S")))
+    return a
 
+
+def copy_files(files_dates: list, target_folder: str, source_folder: str) -> None:
+    for name, date in files_dates:
+        date_str = "{:02d}.{:02d}.".format(date.month, date.day)
+        if not os.path.isdir(os.path.join(target_folder, date_str)):
+            os.makedirs(os.path.join(target_folder, date_str))
+        copyfile(os.path.join(source_folder, name), os.path.join(target_folder, date_str, name))
+
+
+if __name__ == "__main__":
+    parser = get_arg_parser()
+    args = parser.parse_args()
